889138f8df6735748af0c1713aed44532104521f
abhaykantmishra <abhaykantmishra004@gmail.com>
2025-03-29 13:20:05 +0530

    fix(ui): minor ui fixes

--- src/app/page.js
+++ src/app/page.js
@@ -31,7 +31,7 @@ export default function HomePage() {
       <main>
         <Header />
         <Hero />
-        {/* <Services /> */}
+        <Services />
         <DigitalTransformation />
         <Industries />
         <CompanyStats /> 
